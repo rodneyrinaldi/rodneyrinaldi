c3f12d3c04160b0b2b6395f3fb13dd21c01e570e
rodneyrinaldi <rodneyrinaldi@hotmail.com>
2020-06-04 18:46:44 -0300

    Update Project Configuration

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,17 +1,17 @@
 import React from "react"
-import SEO from "./src/components/seo"
-import Layout from "./src/components/layout"
+import SEO from "../components/seo"
+import Layout from "../components/layout"
 
-import About from "./src/components/about/About"
-import Avatar from "./src/components/about/Avatar"
-import Profile from "./src/components/about/Profile"
+import About from "../components/about/About"
+import Avatar from "../components/about/Avatar"
+import Profile from "../components/about/Profile"
 
-import Header2 from "./src/components/header2"
-import Nav2 from "./src/components/nav2"
+import Header2 from "../components/header2"
+import Nav2 from "../components/nav2"
 
 
-import Footer from "./src/components/footer"
-import Authorship from "./src/components/authorship"
+import Footer from "../components/footer"
+import Authorship from "../components/authorship"
 
 const AboutPage = () => (
   <Layout title="about">
