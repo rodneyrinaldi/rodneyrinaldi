a259dc8a93100ba0ac4c17fad9871ecbd88a2d6a
rodneyrinaldi <rodneyrinaldi@hotmail.com>
2020-10-02 12:43:42 -0300

    Fix url navigations

---
+++ src/pages/forms.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+import Layout from "../components/wrapper"
+
+const FDivorcio = () => (
+  <Layout>
+    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeRIY6NrKBRfu4RoCcJXqUWL3459S7nE3bOIueY-Tn-x-kjQw/viewform?embedded=true" width="640" height="500" frameborder="0" marginheight="0" marginwidth="0">
+      Loading…
+    </iframe>
+  </Layout>
+)
+
+export default FDivorcio
+
+
+
+
