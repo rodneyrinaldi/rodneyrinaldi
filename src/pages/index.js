dafa11b89b1d5c1af7ad53ca34d63c168d739b20
rodneyrinaldi <rodneyrinaldi@hotmail.com>
2021-01-15 17:40:17 -0300

    feat: add landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,65 +1,46 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Image from 'next/image'
+
+import styles from '../styles/home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
-        <title>Create Next App</title>
+        <title>Rodney Rinaldi Page</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <main className={styles.mainLayer}>
+        <div className={styles.rrLogo}>
+          <Image
+            src="/rodneyrinaldi.svg"
+            alt="rodney rinaldi"
+            layout="intrinsic"
+            width={780}
+            height={135}
+          />
         </div>
+
+        <nav>
+          <ul>
+            <li>adv</li>
+            {' | '}
+            <li>dev</li>
+            {' | '}
+            <li>dpo</li>
+          </ul>
+        </nav>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+      <nav>
+
+      </nav>
+
+      <footer>
+
       </footer>
-    </div>
+
+    </>
   )
 }
