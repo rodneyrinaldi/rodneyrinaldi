06ca4e0cdbf1ca81ca3fe267291f7ada8c934f14
rodneyrinaldi <rodneyrinaldi@hotmail.com>
2021-01-20 18:51:22 -0300

    chore: update technology page

--- src/components/technology/index.js
+++ src/components/technology/index.js
@@ -21,24 +21,51 @@ export default function Technology() {
       <main className='stripLight'>
         <div className={styles.tierMain}>
 
-          <h3>Tecnologia</h3>
-          <p>xxxx</p>
+          <h3>Foco no Cliente</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas aperiam qui neque. Ipsam tenetur, odio, excepturi voluptates quam ullam natus neque incidunt corporis iure deleniti aliquam aliquid minima voluptatum necessitatibus.</p>
 
-          <Image
-            src="/customer.svg"
-            alt="foto rodney rinaldi"
-            layout="intrinsic"
-            width={1800}
-            height={703}
-          />
+          <div className={styles.tierArticles}>
+            <div className={styles.divImg}>
+              <Image
+                src="/dev.svg"
+                alt="customer centricity"
+                layout="intrinsic"
+                width={1800}
+                height={703}
+              />
+            </div>
+            <div className={styles.divImg}>
+              <Image
+                src="/dev.svg"
+                alt="customer centricity"
+                layout="intrinsic"
+                width={1800}
+                height={703}
+              />
+            </div>
+          </div>
 
-          <Link href='/'>
-            <a href='#' className='buttonBase colorDark'>VOLTAR</a>
-          </Link>
+          <h3>Foco no Cliente</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit odit, voluptas eaque perferendis incidunt nemo pariatur. Totam earum fuga beatae! Aperiam esse dolore cumque quas voluptatum dolor omnis quos consequatur.</p>
 
+          <div className={styles.divImg}>
+            <Image
+              src="/technology.png"
+              alt="customer centricity"
+              layout="intrinsic"
+              width={1800}
+              height={703}
+            />
+          </div>
 
         </div>
       </main>
+
+      <footer className='stripLight'>
+        <Link href='/'>
+          <a href='#' className='buttonBase colorDark'>VOLTAR</a>
+        </Link>
+      </footer>
     </>
   )
 }
