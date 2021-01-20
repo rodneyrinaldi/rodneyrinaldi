46f9d27729ede431c0863b57aed5cf7c93060745
rodneyrinaldi <rodneyrinaldi@hotmail.com>
2021-01-20 13:51:53 -0300

    chore: update technology page

--- src/components/technology/index.js
+++ src/components/technology/index.js
@@ -9,17 +9,15 @@ export default function Technology() {
       <div className='stripLight'>
         <div className={styles.tierMain}>
 
-          <div>
-            <Image
-              src="/technology.png"
-              alt="foto rodney rinaldi"
-              layout="intrinsic"
-              width={1800}
-              height={703}
-            />
-            <h3>Tecnologia</h3>
-            <p>xxxx</p>
-          </div>
+          <Image
+            src="/technology.png"
+            alt="foto rodney rinaldi"
+            layout="intrinsic"
+            width={1800}
+            height={703}
+          />
+          <h3>Tecnologia</h3>
+          <p>xxxx</p>
 
           <Link href="/">
             <a href="#" class={styles.myButton}>VOLTAR</a>
