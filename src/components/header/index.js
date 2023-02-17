b820dc63df5c4d3c46ae948b9687896fabd1b2b0
rodneyrinaldi <rodneyrinaldi@hotmail.com>
2023-02-17 19:59:55 -0300

    first commit

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,22 +1,28 @@
-import React from 'react';
-import Head from 'next/head'
+import Head from "next/head";
+import React from "react";
 
-import styles from './index.module.css'
+import styles from "./index.module.css";
 
 function Header(props) {
-  return (<>
-    <Head>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <title>blog.rodneyrinaldi</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-    <div className={styles.container}>
-      <div >
-        <img src="/rodneyrinaldi.svg" alt="rodneyrinaldi" className={styles.logo2} />
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <title>rodneyrinaldi</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className={styles.container}>
+        <div>
+          <img
+            src="/rodneyrinaldi.svg"
+            alt="rodneyrinaldi"
+            className={styles.logo2}
+          />
+        </div>
       </div>
-    </div>
-  </>)
+    </>
+  );
 }
 
-export default Header
+export default Header;
